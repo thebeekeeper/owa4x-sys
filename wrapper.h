1fba319b989346e29b668e976a508595a60fcdc5
Nick Gamroth <thebeekeeper@gmail.com>
2022-12-20 13:58:27 -0600

    updated dependencies for v2 library

--- wrapper.h
+++ wrapper.h
@@ -1,13 +1,13 @@
 
-#include "/opt/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/lib/gcc/arm-linux-gnueabihf/5.3.1/include/owa4x/owerrors.h"
-#include "/opt/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/lib/gcc/arm-linux-gnueabihf/5.3.1/include/owa4x/owcomdefs.h"
-#include "/opt/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/lib/gcc/arm-linux-gnueabihf/5.3.1/include/owa4x/INET_ModuleDefs.h"
-#include "/opt/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/lib/gcc/arm-linux-gnueabihf/5.3.1/include/owa4x/pm_messages.h"
-#include "/opt/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/lib/gcc/arm-linux-gnueabihf/5.3.1/include/owa4x/GPS2_ModuleDefs.h"
-#include "/opt/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/lib/gcc/arm-linux-gnueabihf/5.3.1/include/owa4x/IOs_ModuleDefs.h"
-#include "/opt/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/lib/gcc/arm-linux-gnueabihf/5.3.1/include/owa4x/RTUControlDefs.h"
- #include "/opt/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/lib/gcc/arm-linux-gnueabihf/5.3.1/include/owa4x/GSM_ModuleDefs.h"
+#include "owa4x/owerrors.h"
+#include "owa4x/owcomdefs.h"
+#include "owa4x/INET_ModuleDefs.h"
+#include "owa4x/pm_messages.h"
+#include "owa4x/GPS2_ModuleDefs.h"
+#include "owa4x/IOs_ModuleDefs.h"
+#include "owa4x/RTUControlDefs.h"
+#include "owa4x/GSM_ModuleDefs.h"
 
 // FMS_Defs.h has functions which use default arguments which aren't supported by our compiler.  Not sure how that ought to work
 //#include <time.h>
-//#include "/opt/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/lib/gcc/arm-linux-gnueabihf/5.3.1/include/owa4x/FMS_Defs.h"
+//#include "owa4x/FMS_Defs.h"
